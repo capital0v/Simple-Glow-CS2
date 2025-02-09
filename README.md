f4c2fef7f4937b3bd837af4331c8332ac26f6d71
Danil <danyacapital@gmail.com>
2025-02-09 16:58:27 +0300

    SimpleGlow 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Simple Glow CS2
- Glow hack for Counter Strike 2
+**Glow hack for Counter Strike 2**
+
+## Disclaimer
+<div style="text-align: center;">
+    <strong>If you get caught cheating on VAC protected servers or competitive leagues, that is your responsibility, not mine. I am not responsible for your actions. The code is published for educational purposes only.</strong>
+</div>
+
+## Images 
+
+![preview #1](Images/1.png)
+![preview #2](Images/2.png)
+
+## License
+[MIT](https://github.com/capital0v/Vexium-Counter-Strike-Source/blob/main/LICENSE)
---
+++ Simple Glow/Feature.cs
@@ -0,0 +1,73 @@
+﻿using Capitalov;
+using System.Runtime.InteropServices;
+
+namespace SimpleGlow
+{
+    internal class Feature
+    {
+
+        #region Import
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        #endregion
+
+        private MemoryRaider _memory = new MemoryRaider();
+
+        private nint _client;
+        private nint _instruction;
+
+        private byte[] _realBytes = new byte[] { 0x32, 0xC0 };
+
+        private bool _glowEnabled;
+
+        private const int VK_F4 = 0x73;
+
+        public Feature()
+        {
+            try
+            {
+                _memory.Inject("cs2");
+                _client = _memory.GetModuleBase("client.dll");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+
+            Start();
+        }
+
+        private void Start()
+        {
+            _instruction = _client + 0x867D50;
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                _memory.WriteBytes(_instruction, _realBytes);
+            };
+
+            while (true)
+            {
+                if ((GetAsyncKeyState(VK_F4) & 0x8000) != 0)
+                {
+                    _glowEnabled = !_glowEnabled;
+
+                    if (_glowEnabled)
+                    {
+                        _memory.Nop(_instruction, _realBytes.Length);
+                    }
+                    else
+                    {
+                        _memory.WriteBytes(_instruction, _realBytes);
+                    }
+
+                    Thread.Sleep(300);
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
